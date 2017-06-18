fbbb8b78964ca197d10220181e5b2ac20f6e3249
camp191 <thanapat423@gmail.com>
2017-06-18 23:56:53 +0700

    move slider data to modules folder

--- src/components/Main/ExampleSlider.jsx
+++ src/components/Main/ExampleSlider.jsx
@@ -1,37 +1,7 @@
 import React from 'react'
 import Slider from 'react-slick'
 import SliderList from './SliderList'
-
-let sliderData = [
-    {
-        name: "หน้ากากทักสิโด้",
-        description: "ปกป้องสาวน้อยจากภัยอันตราย",
-        avatar: "http://www.avatarsdb.com/avatars/tuxedo_mask.jpg",
-        title: "เมื่อฉันได้พบคุณ ฉันตกอยู่ในห้วงแห่งความรัก และที่คุณยิ้ม นั่นเป็นเพราะว่าคุณรู้",
-        who: "อาร์ริโก โบอิโต ( Arrigo Boito ) กวีชาวอิตาลี"
-    },
-    {
-        name: "เทนเซ่อเฟส",
-        description: "หน้าตาดี มีตังใช้",
-        avatar: "http://www.avatarsdb.com/avatars/funny_penguin.gif",
-        title: "เมื่อฉันได้พบคุณ ฉันตกอยู่ในห้วงแห่งความรัก และที่คุณยิ้ม นั่นเป็นเพราะว่าคุณรู้",
-        who: "อาร์ริโก โบอิโต ( Arrigo Boito ) กวีชาวอิตาลี"
-    },
-    {
-        name: "แมวโง่วว",
-        description: "ถ้าป๋มน่ารักน้อยกว่าเขาป๋มก็จะไปฮับ",
-        avatar: "http://www.avatarsdb.com/avatars/you_stupid_cat.gif",
-        title: "เมื่อฉันได้พบคุณ ฉันตกอยู่ในห้วงแห่งความรัก และที่คุณยิ้ม นั่นเป็นเพราะว่าคุณรู้",
-        who: "อาร์ริโก โบอิโต ( Arrigo Boito ) กวีชาวอิตาลี"
-    },
-    {
-        name: "Swift",
-        description: "an American singer-songwriter",
-        avatar: "http://www.avatarsdb.com/avatars/taylor_heart.gif",
-        title: "เมื่อฉันได้พบคุณ ฉันตกอยู่ในห้วงแห่งความรัก และที่คุณยิ้ม นั่นเป็นเพราะว่าคุณรู้",
-        who: "อาร์ริโก โบอิโต ( Arrigo Boito ) กวีชาวอิตาลี"
-    },
-]
+import sliderData from './../../modules/SliderData'
 
 var settings = {
     speed: 500,
